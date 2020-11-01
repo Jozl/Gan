23c0d1f3620c2da3010beb0e677b27bfbfada1a3
qzl <zl_qiu@163.com>
2020-11-01 20:04:55 +0800

    11 01  gan扩充数据，5folds算acc

--- Code/utils/classify_helper.py
+++ Code/utils/classify_helper.py
@@ -45,4 +45,50 @@ def print_acc(acc):
 
 def compute_acc(predict_y, test_y, label=None):
     assert len(predict_y) == len(test_y)
-    return sum([1 if p == t and (p == label if label else True) else 0 for p, t in zip(predict_y, test_y)]) / len(test_y)
+    return sum([1 if p == t and (p == label if label else True) else 0 for p, t in zip(predict_y, test_y)]) / len(
+        test_y)
+
+
+def compute_TP_TN_FP_FN(labels_test, labels_predict, label_positive, label_negative):
+    TP, TN, FP, FN = 0, 0, 0, 0
+    for x, y in zip(labels_test, labels_predict):
+        TP += x == y == label_positive
+        TN += x == y == label_negative
+        FP += x == label_negative != y
+        FN += x == label_positive != y
+
+    return TP, TN, FP, FN
+
+
+def compute_classification_indicators(TP, TN, FP, FN):
+    res = (acc_p, acc_n, accuracy, precision, recall, F1, G_mean) = 0, 0, 0, 0, 0, 0, 0
+    try:
+        acc_p = TP / (TP + FN)
+    except ZeroDivisionError:
+        pass
+    try:
+        acc_n = TN / (TN + FP)
+    except ZeroDivisionError:
+        pass
+    try:
+        accuracy = (TP + TN) / (TP + FN + FP + TN)
+    except ZeroDivisionError:
+        pass
+    try:
+        precision = TP / (TP + FP)
+    except ZeroDivisionError:
+        pass
+    try:
+        recall = TP / (TP + FN)
+    except ZeroDivisionError:
+        pass
+    try:
+        F1 = (2 * TP) / (2 * TP + FN + FP)
+    except ZeroDivisionError:
+        pass
+    try:
+        G_mean = ((TP / (TP + FN)) * (TN / (TN + FP))) ** 0.5
+    except ZeroDivisionError:
+        pass
+
+    return acc_p, acc_n, accuracy, precision, recall, F1, G_mean
